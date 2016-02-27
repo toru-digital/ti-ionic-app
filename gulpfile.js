e1888f9d326da3cf799ce6c3907ad6edd262cc5f
Sean Toru <sean@toruinteractive.com>
2016-02-27 23:11:55 +0000

    sass now exports and imports properly

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,23 @@
 var gulp      = require ('gulp');
 var sass      = require ('gulp-sass');
 var minifyCss = require ('gulp-minify-css');
-var yargs     = require ('yargs');
+var yargs     = require ('yargs').argv;
 var rename    = require ('gulp-rename');
+var gulpif    = require ('gulp-if');
 
-require ('./www/lib/ti-ionic/gulp-tasks/scripts')(gulp, yargs.argv);
+require ('./www/lib/ti-ionic/gulp-tasks/scripts')(gulp, yargs);
+var IS_RELEASE_BUILD  = yargs.r || yargs.release;
 
 gulp.task (
   'sass',
-  function (done) {
-    gulp.src ('./scss/ionic.app.scss')
+  function () {
+    return gulp.src ('./scss/ionic.app.scss')
       .pipe (sass ())
       .on ('error', sass.logError)
-      .pipe (minifyCss ({keepSpecialComments: 0}))
-      .pipe (rename ({extname: '.min.css'}))
       .pipe (gulp.dest ('./www/css/'))
-      .on ('end', done);
+      .pipe (gulpif (IS_RELEASE_BUILD, minifyCss ({keepSpecialComments: 0})))
+      .pipe (rename ({extname: '.min.css'}))
+      .pipe (gulp.dest ('./www/css/'));
   });
 
 gulp.task (
