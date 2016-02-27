a63e2b0d479ee0e993a704157098231afa329e82
Sean Toru <sean@toruinteractive.com>
2016-02-27 20:39:07 +0000

    css watch

--- gulpfile.js
+++ gulpfile.js
@@ -94,7 +94,7 @@ gulp.task('sass', function(done) {
 
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
-  gulp.watch(paths.tiIonic, ['ti-ionic']);
+  //gulp.watch(paths.tiIonic, ['ti-ionic']);
 });
 
 gulp.task('install', ['git-check'], function() {
