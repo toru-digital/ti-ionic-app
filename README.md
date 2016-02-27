64ed9a833257507aaae8ac07cbe554310c3e510f
Sean Toru <sean@toruinteractive.com>
2016-02-27 21:20:37 +0000

    readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ti-ionic-dev-app
+
+*add bower and npm dependencies*
+
+`bower install ng-cordova-oauth ngCordova --save-dev`
+
+`npm install gulp gulp-concat gulp-if gulp-jshint gulp-minify-css gulp-ng-html2js gulp-rename gulp-sass gulp-strip-debug gulp-uglify gulp-util jshint jshint-stylish yargs --save`
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,9 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var bower = require('bower');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
-var sh = require('shelljs');
 var yargs = require ('yargs').argv;
 var ngHtml2Js = require ("gulp-ng-html2js");
 var sort = require ('gulp-sort');
@@ -108,28 +106,6 @@ gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass', 'ti-sass']);
   gulp.watch(paths.tiIonic, ['ti-ionic']);
 });
-
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
-});
-
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
-});
-
-
 
 // gulp.task (
 //   'ti-sass',
