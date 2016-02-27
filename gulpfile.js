7736a51f3c0dbf80c28a83e63523643cabb79a3a
Sean Toru <sean@toruinteractive.com>
2016-02-27 19:42:38 +0000

    watch paths

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,10 @@ var rename = require('gulp-rename');
 var sh = require('shelljs');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: [
+    './scss/**/*.scss',
+    './www/lib/ti-ionic/**/*.scss',
+  ]
 };
 
 gulp.task('default', ['sass']);
